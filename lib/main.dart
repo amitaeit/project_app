a6dab3ccc907c537d9f82a3751bb1a45442acade
amitaeit <amitaeit@gmail.com>
2022-09-27 15:14:11 +0530

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -49,10 +49,17 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String? password;
+  String? email;
 
   TextEditingController textController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+
+  static OutlineInputBorder outlineInputBorder = OutlineInputBorder(borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(
+          width:2,
+          color: Colors.green));
 
   String textValue="Hello";
 
@@ -79,54 +86,74 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text("My First App"),
+        title: Text("My Second App"),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
 
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
 
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextFormField(
-              controller: emailController,
-              decoration: InputDecoration(
-                border:OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide:
-                )
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+
+
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  border:outlineInputBorder,
+                  disabledBorder: outlineInputBorder,
+                  focusedBorder: outlineInputBorder,
+                  focusedErrorBorder: outlineInputBorder.copyWith(borderSide: const BorderSide(color: Colors.blue)),
+                  hintText: "Email"
+                ),
+
               ),
-            ),
-            TextFormField(
-              controller: passwordController
-            )
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: passwordController,
+                  decoration: InputDecoration(
+                      border:outlineInputBorder,
+                      disabledBorder: outlineInputBorder,
+                      focusedBorder: outlineInputBorder,
+                      focusedErrorBorder: outlineInputBorder.copyWith(borderSide: const BorderSide(color: Colors.blue)),
+                      label: const Text(
+                        "Password"
+                      )
+
+                  )
+              ),
+              TextButton(
+                  onPressed: (){
+                    email = emailController.text;
+                    password = passwordController.text;
+                    print(email);
+                    print(password);
+                  },
+                  child: const Text("Login")
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          setState(() {
-            textValue=textController.text;
-          });
-          },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
